c8ca4b31227f055d30fade445b3a5746416d158b
Noah Fontes <nfontes@cynigram.com>
2009-01-24 02:41:32 -0500

    Move serialized packet transformations to its own code

--- include/nssd/protocol/packet.h
+++ include/nssd/protocol/packet.h
@@ -13,46 +13,8 @@
 #include <nssd/nssd.h>
 #include <nssd/protocol.h>
 
-/* Packet format:
- *
- * 0       8       16      24      32
- * +-------------------------------+
- * | version       | field count   | <-- Header
- * +---------------+---------------+
- * | size                          |
- * +-------------------------------+
- * +-------------------------------+
- * | type          | size          | <-- Fields
- * +---------------+---------------+
- * | data                          |
- * |               .               |
- * |               .               |
- * |               .               |
- * +-------------------------------+
- */
-
-/* Protocol changes:
- * - 100: Initial version.
- */
-#define NSSD_PROTOCOL_PACKET_VERSION 100
-
-/* All packets are at least the size of the header, which is 8 bytes. */
-#define NSSD_PROTOCOL_PACKET_HEADER_SIZE 8
-
-/* Additionally, each field has an overhead of 4 bytes. */
-#define NSSD_PROTOCOL_PACKET_FIELD_HEADER_SIZE 4
-
-/* Offsets for reading the structures. */
-#define NSSD_PROTOCOL_PACKET_HEADER_OFFSET_VERSION 0
-#define NSSD_PROTOCOL_PACKET_HEADER_OFFSET_COUNT 2
-#define NSSD_PROTOCOL_PACKET_HEADER_OFFSET_SIZE 4
-
-#define NSSD_PROTOCOL_PACKET_FIELD_HEADER_OFFSET_TYPE 0
-#define NSSD_PROTOCOL_PACKET_FIELD_HEADER_OFFSET_SIZE 2
-
 typedef struct nssd_protocol_packet nssd_protocol_packet_t;
 typedef struct nssd_protocol_packet_field nssd_protocol_packet_field_t;
-typedef struct nssd_protocol_packet_serial nssd_protocol_packet_serial_t;
 
 struct nssd_protocol_packet {
   uint16_t field_count;
@@ -66,11 +28,6 @@ struct nssd_protocol_packet_field {
   void *data;
 };
 
-struct nssd_protocol_packet_serial {
-  size_t length;
-  char *data;
-};
-
 nssd_protocol_packet_t *nssd_protocol_packet_allocate(void);
 void nssd_protocol_packet_initialize(nssd_protocol_packet_t *);
 void nssd_protocol_packet_fields_initialize(nssd_protocol_packet_t *, uint8_t);
@@ -78,13 +35,6 @@ void nssd_protocol_packet_field_initialize(nssd_protocol_packet_t *, int);
 void nssd_protocol_packet_field_finalize(nssd_protocol_packet_t *, int);
 void nssd_protocol_packet_finalize(nssd_protocol_packet_t *);
 void nssd_protocol_packet_free(nssd_protocol_packet_t *);
-
-nssd_protocol_packet_serial_t *nssd_protocol_packet_serial_allocate(void);
-void nssd_protocol_packet_serial_initialize(nssd_protocol_packet_serial_t *);
-void nssd_protocol_packet_serial_serialize(nssd_protocol_packet_serial_t *, const nssd_protocol_packet_t *);
-void nssd_protocol_packet_serial_unserialize(nssd_protocol_packet_serial_t *, nssd_protocol_packet_t *);
-void nssd_protocol_packet_serial_finalize(nssd_protocol_packet_serial_t *);
-void nssd_protocol_packet_serial_free(nssd_protocol_packet_serial_t *);
 
 static inline size_t nssd_protocol_packet_fields_length(nssd_protocol_packet_t *packet) {
   assert(packet);
---
+++ lib/nssd/common/protocol/serial_packet.c
@@ -0,0 +1,114 @@
+/* serial_packet.h: Serialization routines for packets.
+ *
+ * This file is part of the nss-daemon NSS backend.
+ * Copyright (c) 2008-2009 Transtruct. All rights reserved.
+ *
+ * This file is released under the terms of the LICENSE document included with
+ * this distribution.
+ */
+
+#include <nssd/protocol/serial_packet.h>
+
+nssd_protocol_serial_packet_t *nssd_protocol_serial_packet_allocate(void) {
+  nssd_protocol_serial_packet_t *serial = malloc(sizeof(nssd_protocol_serial_packet_t));
+  assert(serial);
+
+  return serial;
+}
+
+void nssd_protocol_serial_packet_initialize(nssd_protocol_serial_packet_t *serial) {
+  assert(serial);
+
+  serial->length = -1;
+  serial->body_length = -1;
+  serial->data = NULL;
+}
+
+void nssd_protocol_serial_packet_serialize(nssd_protocol_serial_packet_t *serial, const nssd_protocol_packet_t *packet) {
+  assert(serial);
+  assert(packet);
+
+  size_t size;
+  int i;
+
+  /* How big do we need the packet to be? */
+  size = NSSD_PROTOCOL_PACKET_FIELD_HEADER_SIZE * packet->field_count;
+  for(i = 0; i < packet->field_count; i++) {
+    size += packet->fields[i].length;
+  }
+
+  /* Okay, now allocate it. */
+  serial->length = size + NSSD_PROTOCOL_PACKET_HEADER_SIZE;
+  serial->body_length = size;
+  serial->data = malloc(serial->length);
+
+  /* And copy the data into it. */
+  *(uint16_t *)(serial->data + NSSD_PROTOCOL_PACKET_HEADER_OFFSET_VERSION) =
+    htons((uint16_t)NSSD_PROTOCOL_PACKET_VERSION);
+  *(uint8_t *)(serial->data + NSSD_PROTOCOL_PACKET_HEADER_OFFSET_COUNT) =
+    (uint8_t)packet->field_count;
+  *(uint32_t *)(serial->data + NSSD_PROTOCOL_PACKET_HEADER_OFFSET_SIZE) =
+    htonl((uint32_t)size);
+
+  {
+    char *p;
+    for(i = 0, p = serial->data + NSSD_PROTOCOL_PACKET_HEADER_SIZE;
+        i < packet->field_count;
+        i++, p += packet->fields[i].length + NSSD_PROTOCOL_PACKET_FIELD_HEADER_SIZE) {
+      *(uint16_t *)(p + NSSD_PROTOCOL_PACKET_FIELD_HEADER_OFFSET_TYPE) =
+        htons((uint16_t)packet->fields[i].type);
+      *(uint16_t *)(p + NSSD_PROTOCOL_PACKET_FIELD_HEADER_OFFSET_SIZE) =
+        htons((uint16_t)packet->fields[i].length);
+      /* The real data! */
+      memcpy(p + NSSD_PROTOCOL_PACKET_FIELD_HEADER_SIZE, packet->fields[i].data, packet->fields[i].length);
+    }
+  }
+}
+
+void nssd_protocol_serial_packet_unserialize_header(nssd_protocol_serial_packet_t *serial, nssd_protocol_packet_t *packet, const char *header) {
+  assert(serial);
+  assert(packet);
+  assert(header);
+
+  /* Make sure the packet version matches. */
+  assert(NSSD_PROTOCOL_PACKET_VERSION_GET(&header) == (uint16_t)NSSD_PROTOCOL_PACKET_VERSION);
+
+  nssd_protocol_packet_fields_initialize(packet, NSSD_PROTOCOL_PACKET_COUNT_GET(&header));
+
+  serial->body_length = NSSD_PROTOCOL_PACKET_SIZE_GET(&header);
+  serial->length = NSSD_PROTOCOL_PACKET_HEADER_SIZE + serial->body_length;
+}
+
+void nssd_protocol_serial_packet_unserialize_body(nssd_protocol_serial_packet_t *serial, nssd_protocol_packet_t *packet, const char *body) {
+  /* Make sure we can legitimately read through this. */
+  assert(serial->body_length > packet->field_count * NSSD_PROTOCOL_PACKET_FIELD_HEADER_SIZE);
+
+  int i;
+  char *p;
+  for(i = 0, p = (char *)body;
+      i < packet->field_count;
+      p += NSSD_PROTOCOL_PACKET_FIELD_HEADER_SIZE + packet->fields[i].length) {
+    packet->fields[i].type = ntohs(*(uint16_t *)(p + NSSD_PROTOCOL_PACKET_FIELD_HEADER_OFFSET_TYPE));
+    packet->fields[i].length = ntohs(*(uint16_t *)(p + NSSD_PROTOCOL_PACKET_FIELD_HEADER_OFFSET_SIZE));
+    /* Make sure this field header won't make us read out of bounds. */
+    assert((p - body) + packet->fields[i].length < serial->body_length);
+    
+    nssd_protocol_packet_field_initialize(packet, i);
+    memcpy(packet->fields[i].data, (void *)p + NSSD_PROTOCOL_PACKET_FIELD_HEADER_SIZE, packet->fields[i].length);
+  }
+}
+
+void nssd_protocol_serial_packet_finalize(nssd_protocol_serial_packet_t *serial) {
+  assert(serial);
+
+  if(serial->data)
+    free(serial->data);
+  serial->length = -1;
+  serial->body_length = -1;
+}
+
+void nssd_protocol_serial_packet_free(nssd_protocol_serial_packet_t *serial) {
+  assert(serial);
+
+  free(serial);
+}
